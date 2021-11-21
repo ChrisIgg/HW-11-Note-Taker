f4cebab65865216a2ca58e6ba0e328f5602d0cca
Chris Insignares <insignaresc115@gmail.com>
2021-11-21 17:25:00 -0500

    SAll notes routes completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { parse } = require("path/posix");
 const { clog } = require("./middleware/clog");
 
 const app = express();
@@ -28,6 +29,27 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) =>
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+    var parsedString = JSON.parse(data);
+    res.json(parsedString);
+  })
+);
+// fs.readFile('data.csv', 'utf8', (error, data) => error ? console.error(error) : console.log(data)
+// );
+
+// Uncomment this next function to write to the file with anything you pass in as process.argv[2]
+// const userInput = process.argv[2]
+// console.log(userInput)
+// fs.writeFile('log.txt', process.argv[2], (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
