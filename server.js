9de46f301e73854d865cf0b9b42da69d13301303
Chris Insignares <insignaresc115@gmail.com>
2021-11-22 14:54:53 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("path/posix");
-const { clog } = require("./middleware/clog");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
